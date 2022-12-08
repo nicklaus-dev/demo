7be9bc61ac7b35b79ab5a777741c916763fb770c
nicklausliu <nicklausliu1999@gmail.com>
2022-12-08 16:04:10 +0800

    feat: add code server

--- main.go
+++ main.go
@@ -4,48 +4,10 @@ import (
 	"fmt"
 )
 
-type Error interface{
-	Msg() string
-}
-
-type User struct {
-	Name string
-	Age int
-}
-
-func (u User) Msg() string {
-	return "invalid user name"
-}
-
-type Car struct {
-	Brand string
-}
-
-
-func (c Car) Msg() string {
-	return "invalid brand"
-}
-
-func checkMsg[E Error](e E) E {
-	if e.Msg() == "invalid brand" {
-		return e
-	}
-
-	// return empty e
-	return *new(E)
-}
-
 func main() {
-	u := User{
-		Name: "nick",
-	}
-	c := Car{
-		Brand: "benz",
-	}
-	fmt.Printf("before u: %v\n", u) // before u: {nick 0}
-	fmt.Printf("before c: %v\n", c) // before c: {benz}
-	u = checkMsg(u)
-	c = checkMsg(c)
-	fmt.Printf("after u: %v\n", u) // after u: { 0}
-	fmt.Printf("after c: %v\n", c) // after c: {benz}
+	i := 0
+	go func() {
+		i++
+	}()
+	fmt.Println(i)
 }
