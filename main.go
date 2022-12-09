55f52d9e7767cbbb81732025176f117fda9e2f8c
nicklausliu <nicklausliu1999@gmail.com>
2022-12-09 15:20:42 +0800

    feat: add copilot code

--- main.go
+++ main.go
@@ -7,19 +7,3 @@ import (
 func main() {
 	fmt.Println("Hello, World!")
 }
-
-// binary search by copilot
-func binarySearch(arr []int, target int) int {
-	left, right := 0, len(arr)-1
-	for left <= right {
-		mid := left + (right-left)/2
-		if arr[mid] == target {
-			return mid
-		} else if arr[mid] > target {
-			right = mid - 1
-		} else {
-			left = mid + 1
-		}
-	}
-	return -1
-}
